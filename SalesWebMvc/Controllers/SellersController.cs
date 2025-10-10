49dfbb711c628d24e9db3c145fc73ed45ee4d082
Sérgio Soares <ser.vit.deb@gmail.com>
2025-10-10 15:58:18 -0300

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        // GET: SellersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
