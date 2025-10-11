76242b033e8ccddd5da616ca40248db230a81179
Sérgio Soares <ser.vit.deb@gmail.com>
2025-10-11 16:41:57 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,6 +20,7 @@ public class SellerService
 
     public void Insert(Seller obj)
     {
+        obj.Department = _context.Department.First();
         _context.Add(obj);
         _context.SaveChanges();
     }
